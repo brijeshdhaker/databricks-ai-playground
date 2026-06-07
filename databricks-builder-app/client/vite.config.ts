4fbcec7c2bafc0c6a00f2392c6031e9fb93fab1c
Brijesh K Dhaker <brijeshdhaker@gmail.com>
2026-06-07 19:18:54 +0530

    first commit

---
+++ databricks-builder-app/client/vite.config.ts
@@ -0,0 +1,27 @@
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        headers:{
+          "X-Forward":"SANDBOX"
+        }
+      },
+    },
+  },
+  build: {
+    outDir: 'out',
+  },
+});
